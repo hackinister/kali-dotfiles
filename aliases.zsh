4be5015ee3e48fb129aa1ad9c2a324943afe8717
hackinister <hackinister@protonmail>
2025-05-21 16:28:17 +0200

    Initial Commit

---
+++ aliases.zsh
@@ -0,0 +1,19 @@
+# Base
+alias sudo='sudo '
+alias c='clear'
+alias r='source ~/.zshrc'
+# Programs
+alias vim='nvim'
+alias server='python3 -m http.server 80'
+alias listen='nc -nvlp 4242'
+
+# Utils & alternatives
+alias cat='batcat'
+
+alias apt='nala'
+alias upgrade='sudo nala upgrade'
+alias install='sudo nala install'
+
+# Hacking
+alias hack='cd /home/kali/offsec && sudo openvpn universal.ovpn'
+
