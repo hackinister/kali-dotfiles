9179138bb08f26cf5d7afa2ee056b213e073be9c
hackinister <hackinister@protonmail>
2025-11-26 11:16:15 +0100

    New Dotfiles

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -17,17 +17,26 @@ alias apt=nala
 alias diff='diff --color=auto'
 alias hack='cd /home/kali/offsec && sudo openvpn universal.ovpn'
 alias install='sudo nala install'
-alias ip='ip --color=auto'
-alias listen='nc -nvlp 4242'
+alias ip='ip -c'
+alias ipa='ip -c -br a'
+alias listen='rlwrap nc -nvlp'
 alias server='python3 -m http.server 80'
+alias serverPort='python3 -m http.server'
 alias sudo='sudo '
 alias update='sudo nala update'
 alias upgrade='sudo nala full-upgrade'
 alias vim=nvim
+
+# chezmoi Aliase
+alias cm='chezmoi'
+alias cme='chezmoi edit'
+alias cmap='chezmoi apply'
+alias cmad='chezmoi add'
 
 # fzf Integration
 set -gx FZF_DEFAULT_COMMAND 'fdfind --type f'
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -gx FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border'
 
+set -gx fish_greeting ""
 starship init fish | source
