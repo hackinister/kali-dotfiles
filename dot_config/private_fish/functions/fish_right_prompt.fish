73161efc18fe9c346415ff3193a149e0bb51b815
hackinister <hackinister@protonmail>
2025-12-20 22:13:44 +0100

    tmux, nvim and alacritty themes

--- dot_config/private_fish/functions/fish_right_prompt.fish
+++ dot_config/private_fish/functions/fish_right_prompt.fish
@@ -2,5 +2,5 @@ function fish_right_prompt
     set -l tun0_addr (tun0_ip)
     set -l utc_time (date -u +"%H:%M UTC")
 
-    string join '' -- "[" (set_color -o yellow) $utc_time (set_color normal) "● " (set_color -o magenta) \($tun0_addr\) (set_color normal) "]"
+    string join '' -- (set_color normal) "[" (set_color -o yellow) $utc_time (set_color normal) "● " (set_color -o magenta) \($tun0_addr\) (set_color normal) "]"
 end
