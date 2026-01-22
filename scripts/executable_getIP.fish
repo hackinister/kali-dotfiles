26c68aebad6426a20e01a42d13e228102695a50a
hackinister <hackinister@protonmail>
2026-01-22 13:12:03 +0100

    Update scripts/getIP.fish

--- scripts/executable_getIP.fish
+++ scripts/executable_getIP.fish
@@ -1,9 +1,9 @@
 #!/bin/fish
-set ip (ip -4 -o a show tun0 2>/dev/null | awk '{print $4}' | cut -d/ -f1)
+set ip_addr (ip -4 -o a show tun0 2>/dev/null | awk '{print $4}' | cut -d/ -f1)
 
-if ip -n "$ip"
-	echo " $ip"
-	set -Ux LIP "$ip"
+if test -n "$ip_addr"
+	echo " $ip_addr"
+	set -Ux LIP "$ip_addr"
 else
 	echo "   "
 end
