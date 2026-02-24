1fce93f5c68be1580bf30962f9c2b6c1388bd0c0
hackinister <hackinister@protonmail>
2026-02-24 20:25:29 +0100

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,10 +1,15 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-
+local mux = wezterm.mux
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	pane:split({ size = 0.5 })
+end)
 
 config.colors = {
 	foreground = "#CBE0F0",
