97f44492ab3f69d0bfc95bdd652df6476d42117a
hackinister <hackinister@protonmail>
2026-03-04 21:16:45 +0100

    Add .config/fish/functions/r.fish

---
+++ dot_config/private_fish/functions/r.fish
@@ -0,0 +1,3 @@
+function r --wraps='source /home/kali/.config/fish/config.fish' --description 'alias r=source /home/kali/.config/fish/config.fish'
+    source /home/kali/.config/fish/config.fish $argv
+end
